ff5df911df8fd6de3ed2ecdc46bde9851b4ae3db
Fabian Wiesel <fabian.wiesel@sap.com>
2022-10-18 16:16:48 +0200

    Mount all partitions and execute in chroot

--- ironic_python_agent_sapcc_extensions/__init__.py
+++ ironic_python_agent_sapcc_extensions/__init__.py
@@ -12,27 +12,124 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import contextlib
+import os
+import tempfile
+import textwrap
+from urllib.parse import urlparse
+
+from oslo_concurrency import processutils
 from oslo_log import log
 
 from ironic_python_agent.extensions import base
+from ironic_python_agent.extensions import image
 from ironic_python_agent import utils
 
 LOG = log.getLogger(__name__)
 
 
+def _mount_partition(partition, path):
+    if not os.path.ismount(path):
+        LOG.debug('Attempting to mount %(device)s to %(path)s to '
+                  'partition.',
+                  {'device': partition,
+                   'path': path})
+        try:
+            utils.execute('mount', partition, path)
+        except processutils.ProcessExecutionError as e:
+            # NOTE(TheJulia): It seems in some cases,
+            # the python os.path.ismount can return False
+            # even *if* it is actually mounted. This appears
+            # to be becasue it tries to rely on inode on device
+            # logic, yet the rules are sometimes different inside
+            # ramdisks. So lets check the error first.
+            if 'already mounted' not in e:
+                # Raise the error, since this is not a known
+                # failure case
+                raise
+            else:
+                LOG.debug('Partition already mounted, proceeding.')
+
+
 class SapCc(base.BaseAgentExtension):
-    @base.sync_command('run')
-    def run(self):
+    MOUNT_PATH = "/mnt"
+
+    @base.sync_command('install_vsmp_memoryone')
+    def install_vsmp_memoryone(self, **kwargs):
         """Yadda yadda"""
         instance_info = self.agent.node.get("instance_info", {})
+        LOG.debug("Node: %s", self.agent.node)
         traits = instance_info.get("traits", [])
         if "CUSTOM_VSMP_MEMORYONE" not in traits:
-            return {"status": "required trait missing"}
-        image_properties = instance_info.get("image_properties")
-        if not image_properties:
-            return {"status": "image property missing"}
+            return {"info": "required trait missing"}
+        image_properties = instance_info.get("image_properties", {})
+        vsmp_version = image_properties.get("sapcc.vsmp_version", "latest")
 
-        url = image_properties.get("sapcc.post_install_script_url")
-        log = utils.get_command_output(["bash", "-c", "curl -sSL " + url + " | bash"])
-        compact = utils.gzip_and_b64encode(io_dict={"log": log})
-        return {"log": compact, "status": "success"}
+        image_url = image_properties.get("direct_url")
+        if not image_url:
+            return {"info": "no image_url"}
+
+        image_url = urlparse(image_url)
+        if not image_url.netloc:
+            return {"info": "could no parse image_url"}
+
+        domain = image_url.netloc.split(".", 1)[1]
+        url = (f"https://repo.{domain}/memoryone/"
+               f"{{vsmp_installer-{vsmp_version}.sh,license.txt}}")
+
+        utils.execute('mount', '-t', 'efivarfs', 'efivarfs',
+                      '/sys/firmware/efi/efivars')
+
+        with tempfile.TemporaryDirectory() as path:
+            script_path = f"{path}/install.sh"
+            with open(script_path, mode="w", encoding="utf8") as script:
+                script.write(textwrap.dedent(
+                    f"""\
+                    set -Eeuo pipefail
+                    shopt -s nullglob
+                    cd /etc
+                    [ -f resolv.conf ] || ln -s ../run/systemd/resolve/stub-resolv.conf resolv.conf
+                    cd "{path}"
+                    curl --retry 5 -sfZO "{url}"
+                    chmod +x ./vsmp_installer*
+                    ./vsmp_installer* in -q -n *.txt 2>&1
+                    echo "Done"
+                    """))
+
+            with contextlib.ExitStack() as stack:
+                stack.enter_context(self._mount_root())
+                stack.enter_context(self._mount_for_chroot())
+                stack.enter_context(self._mount_tmp_for_chroot())
+                bytes_io = utils.get_command_output([
+                    "chroot", self.MOUNT_PATH, "/usr/bin/bash", script_path])
+                log = bytes_io.read().decode('utf8')
+
+            return {"log": log, "status": "success"}
+
+    @contextlib.contextmanager
+    def _mount_root(self):
+        try:
+            _mount_partition("LABEL=ROOT", self.MOUNT_PATH)
+            yield
+        finally:
+            utils.execute('umount', self.MOUNT_PATH)
+
+    @contextlib.contextmanager
+    def _mount_for_chroot(self):
+        try:
+            image._mount_for_chroot(self.MOUNT_PATH)
+            yield
+        finally:
+            image._umount_all_partitions(
+                self.MOUNT_PATH,
+                path_variable=image._get_path_variable(),
+                umount_warn_msg="")
+
+    @contextlib.contextmanager
+    def _mount_tmp_for_chroot(self):
+        try:
+            utils.execute('mount', '-o', 'bind', '/tmp',
+                          self.MOUNT_PATH + '/tmp')
+            yield
+        finally:
+            utils.execute('umount', self.MOUNT_PATH + '/tmp')
